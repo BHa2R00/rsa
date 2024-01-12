9d34be2f809d7518abc9641516423dae4f3ab710
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2024-01-12 20:12:29 +0800

    Update modexpt_tb.sv

--- tb/modexpt_tb.sv
+++ tb/modexpt_tb.sv
@@ -21,13 +21,16 @@ task reset_p;
 	rstn = 0;
 endtask
 
-parameter I_MSB = 2;
-parameter J_MSB = 10;
+parameter I_MSB = 3;
+parameter J_MSB = 3;
+parameter [J_MSB:0] B_MSB = ((2**(J_MSB+1))-1);
+parameter [B_MSB:0] B_MAX = (2**B_MSB-1);
 
 wire ack;
 reg req;
 wire [(2**(I_MSB+1)-1):0] tx_data;
 reg [(2**(J_MSB+1)-1):0] rx_data_2;
+reg [J_MSB+1:0] rx_data_2_msb;
 reg [(2**(I_MSB+1)-1):0] rx_data_1, rx_data_3;
 reg enable;
 modexpt #(
@@ -38,6 +41,7 @@ modexpt #(
 	.req(req), 
 	.tx_data(tx_data), 
 	.rx_data_2(rx_data_2), 
+	.rx_data_2_msb(rx_data_2_msb), 
 	.rx_data_1(rx_data_1), .rx_data_3(rx_data_3), 
 	.enable(enable), 
 	.rstn(rstn), .clk(clk) 
@@ -59,21 +63,24 @@ task test1;
 	$display("test1 start");
 	enable_p;
 	repeat(10) begin
-		rx_data_1 = $urandom_range(0, 2**(2**(I_MSB+1))-1);
-		rx_data_2 = $urandom_range(1, 2**(2**(J_MSB+1))-1);
-		rx_data_3 = $urandom_range(1, 2**(2**(I_MSB+1))-1);
+		rx_data_1 = $urandom_range(0, 2**(2**(I_MSB+1-1))-1);
+		rx_data_2 = $urandom_range(1, B_MAX);
+		rx_data_2 = (rx_data_2[4:1] == 0) ? 65537 : rx_data_2;
+		rx_data_2_msb = (rx_data_2 == 65537) ? 18 : {1'b0, {(J_MSB+1){1'b1}}};
+		rx_data_3 = $urandom_range(1, 2**(2**(I_MSB+1-1))-1);
 		req = ~req;
 		@(posedge ack);
-		$display("(modexpt %d %d %d) -> %d", rx_data_1, rx_data_2, rx_data_3, tx_data);
+		$display("	(modexpt %d %d %d); //-> %d", rx_data_1, rx_data_2, rx_data_3, tx_data);
 	end
 	enable_n;
 	$display("test1 end");
 endtask
 
 initial begin
-	rx_data_1 = $urandom_range(0, 2**(2**(I_MSB+1))-1);
-	rx_data_2 = $urandom_range(1, 2**(2**(J_MSB+1))-1);
-	rx_data_3 = $urandom_range(1, 2**(2**(I_MSB+1))-1);
+	rx_data_1 = $urandom_range(0, 2**(2**(I_MSB+1-1))-1);
+	rx_data_2 = $urandom_range(1, 2**(2**(J_MSB+1-1))-1);
+	rx_data_2_msb = {1'b0, {(J_MSB+1){1'b1}}};
+	rx_data_3 = $urandom_range(1, 2**(2**(I_MSB+1-1))-1);
 	req = 0;
 	reset_n;
 	repeat(10) test1;
@@ -82,7 +89,7 @@ initial begin
 end
 
 initial begin
-  $dumpfile("../work/modexpt_tb.fst");
+	$dumpfile("../work/modexpt_tb.fst");
 	$dumpvars(0, modexpt_tb);
 end
 
